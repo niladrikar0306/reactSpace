4b7dffcaba5f623db9fac6fcb646d5b9e83c5e3a
niladrikar0306 <niladri.kar0306@gmail.com>
2017-07-30 22:01:23 +0530

    react with redux

---
+++ my-react3-redux/src/js/client.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+
+import Layout from "./pages/Layout";
+
+
+const app = document.getElementById('app');
+ReactDOM.render(
+  <BrowserRouter>
+    <Layout />
+  </BrowserRouter>
+  , app);
