3048c8779ef2faaaddda11e942d0a0815f6fbc7e
niladrikar0306 <niladri.kar0306@gmail.com>
2017-08-18 02:28:37 +0530

    APP without redux

---
+++ my-react6-redux-aml-dashboard/src/app/components/layout/Nav.js
@@ -0,0 +1,62 @@
+import React from "react";
+import PropTypes from 'prop-types'
+import { Link, withRouter } from "react-router-dom";
+
+class Nav extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      collapsed: true,
+    };
+  }
+
+  static propTypes = {
+    match: PropTypes.object.isRequired,
+    location: PropTypes.object.isRequired,
+    history: PropTypes.object.isRequired
+  }
+
+  toggleCollapse() {
+    const collapsed = !this.state.collapsed;
+    this.setState({collapsed});
+  }
+
+  render() {
+    const { match, location, history } = this.props;
+    const { collapsed } = this.state;
+    const homeClass =  location.pathname === "/" ? "active" : "";
+    const dqClass = location.pathname === "/dq" ? "active" : "";
+    const navClass = collapsed ? "collapse" : "";
+
+    console.log(this.props);
+
+    return (
+      <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle" onClick={this.toggleCollapse.bind(this)} >
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+          </div>
+          <div class={"navbar-collapse " + navClass} id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+              <li class={homeClass}>
+                <Link to='/' onClick={this.toggleCollapse.bind(this)}>Home</Link>
+              </li>
+              <li class={dqClass}>
+                <Link to="/dq" onClick={this.toggleCollapse.bind(this)}>DQ</Link>
+              </li>
+
+            </ul>
+          </div>
+        </div>
+      </nav>
+    );
+  }
+}
+
+
+export default withRouter(Nav)
---
+++ my-react6-redux-aml-dashboard/src/app/pages/layout/Layout.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+
+import Nav from "../../components/layout/Nav";
+
+import Home from "../home/Home";
+import DQ from "../dq/DQ";
+
+
+export default class Layout extends React.Component {
+  render() {
+      const { location } = this.props;
+      const containerStyle = {
+        marginTop: "60px"
+      };
+      console.log("layout");
+      console.log(this.props);
+      return (
+        <div>
+
+            <Nav location={location} />
+
+            <div class="container" style={containerStyle}>
+              <div class="row">
+                <div class="col-lg-12">
+                  <h1>AML Dashboard</h1>
+
+                  <Switch>
+                    <Route exact path='/' component={Home}/>
+                    <Route path='/dq' component={DQ}/>
+                  </Switch>
+
+                </div>
+              </div>
+            </div>
+          </div>
+      )
+  }
+}
