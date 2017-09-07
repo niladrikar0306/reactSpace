b377c20218c929da5453a44168060ca6890c1c45
niladrikar0306 <niladri.kar0306@gmail.com>
2017-09-08 00:44:26 +0530

    forgot whats new

--- my-react4-redux/src/js/client.js
+++ my-react4-redux/src/js/client.js
@@ -1,88 +1,49 @@
-import {applyMiddleware, createStore} from "redux"
+import {applyMiddleware, combineReducers, createStore} from "redux"
 import logger from "redux-logger"
-import thunk from "redux-thunk"
-import promise from "redux-promise-middleware"
-import axios from "./axios/axios.js"
 
-const initialState = {
-  fetching : false,
-  fetched : true,
-  users : []
-}
-
-const reducer = function(state, action) {
-
+const usersReducer = function (state={}, action) {
+  console.log("inside usersReducer");
   switch (action.type) {
-    case "USERS_PENDING":
-      // using spread syntax
-      state = {...state, fetching: true}
+    case "CHANGE_NAME":
+      state = {...state, name: action.payload}
       break;
-    case "USERS_FULFILLED":
-      // using spread syntax
-      state = {...state, fetching: false, fetched : true, users : action.payload.data.users}
+    case "CHANGE_AGE":
+    state = {...state, age: action.payload}
+    break;
       break;
-    case "USERS_REJECTED":
-      state = {...state, fetching: false, fetched : false}
-      break;
+    default:
+
   }
-  console.log("action.payload : ", action.payload);
-  // all reducer should return the state
-  return state
+  return state;
 }
 
-// Now create the middleware
-const middleware = applyMiddleware(promise(), thunk, logger)
+const commentsReducer = function (state=[], action) {
+  console.log("inside commentsReducer");
+  return state;
+}
 
-const store = createStore(reducer, initialState, middleware)
+const reducers = combineReducers({
+  users : usersReducer,
+  comments : commentsReducer,
+})
+
+
+const demoState= {
+  users: {
+    name: "Niladri",
+    age: 30
+  },
+  comments: []
+}
+
+const middleware = applyMiddleware(logger);
+
+const store = createStore(reducers, {}, middleware)
 
 store.subscribe(() => {
   console.log("store changed : ", store.getState())
 })
 
-// for success
-store.dispatch({
-  type: "USERS",
-  payload : axios.get("/users")
-})
 
-// for error
-store.dispatch({
-  type: "USERS",
-  payload : axios.get("/usersErr")
-})
-
-// without promise for success
-store.dispatch((dispatch) => {
-  // init load users
-  dispatch({type: "FETCH_USERS_START"})
-  // get loaded users
-  axios.get("/users")
-    .then(response => {
-      dispatch({type: "FETCH_USERS_SUCCESSFUL", payload: response.data})
-    })
-    .catch(err => {
-      dispatch({type: "FETCH_USERS_ERROR", payload: err})
-
-    })
-  // handle loaded users
-
-})
-
-// without promise for error
-store.dispatch((dispatch) => {
-  // init load users
-  dispatch({type: "FETCH_USERS_START"})
-  // get loaded users
-  axios.get("/usersError")
-    .then(response => {
-      console.log(response)
-      dispatch({type: "FETCH_USERS_SUCCESSFUL", payload: response.data})
-    })
-    .catch(err => {
-      console.log(err)
-      dispatch({type: "FETCH_USERS_ERROR", payload: err})
-
-    })
-  // handle loaded users
-
-})
+store.dispatch({type: "CHANGE_NAME", payload: "Niladri"})
+store.dispatch({type: "CHANGE_AGE", payload: 20})
